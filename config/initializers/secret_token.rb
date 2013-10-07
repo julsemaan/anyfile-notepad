b1cf93dc9c89e726a6231d74ecb7fa3d657dcb89
root <root@scentos2.wiprogram.info>
2013-10-06 22:07:55 -0400

    Added session token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,6 @@
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnyfileNotepad::Application.config.secret_token = 'some secret phrase of at least 30 characters that is so agile and so nice'
+
