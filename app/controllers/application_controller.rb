9fa83f82e05e07eece22a5785248a22d13e4ec86
root <root@scentos2.wiprogram.info>
2013-10-10 21:35:39 -0400

    Added session update on each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :execute_default
+  after_filter :execute_after
   
   def execute_default
     @gapi = GApi.new
     api_client = @gapi.client
     
     api_client.authorization.update_token!(session)
+    api_client.authorization.inspect
     if api_client.authorization.refresh_token &&
       api_client.authorization.expired?
       api_client.authorization.fetch_access_token!
@@ -36,5 +38,14 @@ class ApplicationController < ActionController::Base
     end
     
     
+  end
+  
+  def execute_after
+    api_client = @gapi.client
+    
+    session[:access_token] = api_client.authorization.access_token
+    session[:refresh_token] = api_client.authorization.refresh_token
+    session[:expires_in] = api_client.authorization.expires_in
+    session[:issued_at] = api_client.authorization.issued_at
   end
 end
