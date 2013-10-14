933410583532d25e1106a0fbb492ae4bb6e21408
root <root@scentos2.wiprogram.info>
2013-10-14 14:38:20 -0400

    Added oauth code capture to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :capture_auth
+  
+  def capture_auth
+    if params[:code]
+      @to_store = @gapi.authorize_code(params[:code])
+      @to_store.each do |key,value|
+        session[key] = value
+      end
+    elsif params[:error]
+      render :status => :forbidden, :text => "Authorization failed with Google API"
+    end
+  end
 
 end
--- app/controllers/g_oauth_controller.rb
+++ app/controllers/g_oauth_controller.rb
@@ -6,15 +6,6 @@ class GOauthController < ApplicationController
     @gapi = GApi.new
     api_client = @gapi.client
     
-    if params[:code]
-      @to_store = @gapi.authorize_code(params[:code])
-      @to_store.each do |key,value|
-        session[key] = value
-      end
-    elsif params[:error]
-      render :status => :forbidden, :text => "Authorization failed with Google API"
-    end
-    
 
     api_client.authorization.update_token!(session)
     api_client.authorization.inspect
