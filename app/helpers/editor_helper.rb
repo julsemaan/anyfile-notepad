93c4649353d3d721ca0453fcd747b3b074dcdf35
Julien Semaan <julien@ubuntu-js.(none)>
2013-10-28 16:16:09 -0400

    Added menu minimizer with session memorization

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,2 +1,22 @@
 module EditorHelper
+  def prefers_minimized
+    if session[:prefers_minimized].nil?
+      false
+    else
+      session[:prefers_minimized].to_bool
+    end
+  end
+  
+  def show_minimized?(g_file)
+    #puts "Flash : "+flash
+    #puts "Errors : "+g_file.errors
+    if prefers_minimized
+      if flash.empty?
+        if not g_file.errors.any?
+          return true
+        end
+      end
+    end
+    return false
+  end
 end
