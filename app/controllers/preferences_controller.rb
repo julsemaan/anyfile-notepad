566b989b5673206500c4595ce119384cee0d0edd
Julien Semaan <julien@ubuntu-js.(none)>
2013-10-29 18:25:41 -0400

    Font size for the editor in the css to match the preferences. Preferences cookie expires in a year

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,7 +1,7 @@
 class PreferencesController < GOauthController
   def get_update
     @preferences.set_preferences(params)
-    cookies[:preferences] = ActiveSupport::JSON.encode(@preferences.hash)
+    cookies[:preferences] = {:value => ActiveSupport::JSON.encode(@preferences.hash), :expires => 1.year.from_now}
     #@preferences.save
     #puts cookies[:preferences]
     render text: "#{@preferences.hash} #{params}"
