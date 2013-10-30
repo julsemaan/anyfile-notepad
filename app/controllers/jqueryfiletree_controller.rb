3e6ea9d4d0c5b34576ee840b615985fdab032c4d
Julien Semaan <julien@ubuntu-js.(none)>
2013-10-29 22:14:35 -0400

    Corrected the bug where the dropdown wouldn't work when you opened the file explorer

--- app/controllers/jqueryfiletree_controller.rb
+++ app/controllers/jqueryfiletree_controller.rb
@@ -1,8 +1,10 @@
 class JqueryfiletreeController < GOauthController
   protect_from_forgery :only => []
+  
   def content
     @root_id = params[:dir]
     @root_id.slice! '/'
     @dir = Jqueryfiletree.new(@root_id, @gapi).get_content
+    render :layout => false
   end
 end
