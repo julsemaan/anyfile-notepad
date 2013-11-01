c268bb27a8bbac69c100fd3395731b412ca48e07
Julien Semaan <julien@ubuntu-js.(none)>
2013-11-01 12:09:52 -0400

    New unicorn threading handling. Thanks  lucashungaro on github.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.14'
 
 gem 'unicorn'
+gem 'unicorn-worker-killer'
 
 group :development do
   gem 'sqlite3'
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,35 @@
-worker_processes 3
-timeout 10
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout Integer(ENV["UNICORN_TIMEOUT"] || 30)
+ 
 preload_app true
-
+ 
+before_exec do |server|
+  ENV["RUBY_HEAP_MIN_SLOTS"]=800000
+  ENV["RUBY_GC_MALLOC_LIMIT"]=59000000
+  ENV["RUBY_HEAP_SLOTS_INCREMENT"]=10000
+  ENV["RUBY_HEAP_SLOTS_GROWTH_FACTOR"]=1
+  ENV["RUBY_HEAP_FREE_MIN"]=100000
+end
+ 
 before_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  Signal.trap "TERM" do
+    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
+    Process.kill "QUIT", Process.pid
   end
-
+ 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
-
+ 
 after_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+ 
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config["reaping_frequency"] = ENV["DB_REAP_FREQ"] || 10 # seconds
+    config["pool"]              = ENV["DB_POOL"] || 6
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
