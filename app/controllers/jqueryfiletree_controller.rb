e8d422fed2202f876201023705a6f5abebb693e8
Julien Semaan <jul.semaan@gmail.com>
2013-11-11 22:30:00 -0500

    New smooth oauth flow that returns a 401 when api is not authorized. Used by preferences in editor.

--- app/controllers/jqueryfiletree_controller.rb
+++ app/controllers/jqueryfiletree_controller.rb
@@ -1,23 +1,10 @@
 class JqueryfiletreeController < GOauthController
   protect_from_forgery :only => []
-  
-  skip_before_filter :execute_default
-  before_filter :execute_default_smooth
-  
-  def execute_default_smooth
-    @gapi.client.authorization.update_token!(session)
-    @gapi.client.authorization.inspect
-    @gapi.client.authorization.refresh_token
-  end
-   
+    
   def content
-    begin
-      @root_id = params[:dir]
-      @root_id.slice! '/'
-      @dir = Jqueryfiletree.new(@root_id, @gapi).get_content
-      render :layout => false
-    rescue
-      render :status => :forbidden, :text => "Authorization failed."
-    end
+    @root_id = params[:dir]
+    @root_id.slice! '/'
+    @dir = Jqueryfiletree.new(@root_id, @gapi).get_content
+    render :layout => false
   end
 end
