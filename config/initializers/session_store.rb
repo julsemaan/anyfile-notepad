6389083bdc535190e023e7b6d950a4bbce642ace
Julien Semaan <jul.semaan@gmail.com>
2013-12-27 16:38:41 +0000

    Revert "Sessions last for 3 hours"
    This reverts commit 91f50c05968b5933b777c55305698b791b6c3cd3.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AnyfileNotepad::Application.config.session_store :cookie_store, key: '_anyfile_notepad_session', expire_after: 3.hours
+AnyfileNotepad::Application.config.session_store :cookie_store, key: '_anyfile_notepad_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
