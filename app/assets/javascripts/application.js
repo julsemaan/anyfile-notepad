ff839403a9f0fdd5ecc132eb8dfa079dc0afef3d
Julien Semaan <jul.semaan@gmail.com>
2015-10-14 19:03:24 -0400

    add js method to parse get params

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -170,4 +170,13 @@ function createCSSSelector(selector, style) {
     }
 }
 
-
+function getQueryParam(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+}
