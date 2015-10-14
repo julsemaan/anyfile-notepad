ce7c3b5bb523a3a6e14365abcfbc6db46010af96
Julien Semaan <jul.semaan@gmail.com>
2015-10-14 17:13:37 -0400

    make panel header an h4

--- app/assets/javascripts/MDRenderer.js
+++ app/assets/javascripts/MDRenderer.js
@@ -107,7 +107,7 @@ MDRenderer.prototype.createPanels = function() {
 MDRenderer.prototype.createPanel = function(title, panel_items){
   var self = this; 
   var panel = $('<div class="panel"></div>')
-  var panel_header = $('<h3 class="panel-heading"></h3>');
+  var panel_header = $('<h4 class="panel-heading"></h3>');
   var panel_body = $('<div class="panel-body"></div>');
   panel.append(panel_header);
   panel.append(panel_body);
