85c60b43576cf30b546a2752c8769a7fed06d137
Julien Semaan <julien@semaan.ca>
2015-12-21 18:22:42 -0500

    added array preference

---
+++ app/assets/javascripts/Model/Preference/ArrayPreference.js
@@ -0,0 +1,17 @@
+Class("ArrayPreference", ["Preference"]);
+
+ArrayPreference.find = Preference.find;
+
+ArrayPreference.prototype.init_child = function(args){
+  var value = args["value"] || "[]";
+  this.array = JSON.parse(value);
+  this.value = value;
+}
+
+ArrayPreference.prototype.commit = function(locker, fail_action){
+  this.setValue(this.array, locker, fail_action);
+}
+
+ArrayPreference.prototype.toString = function() {
+  return JSON.stringify(this.array);
+}
