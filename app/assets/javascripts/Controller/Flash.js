45396f334a08a14bd2ac956b5cbbdf78aa87e149
Julien Semaan <julien@semaan.ca>
2016-03-06 17:26:37 -0500

    ameliorate mobile flash

--- app/assets/javascripts/Controller/Flash.js
+++ app/assets/javascripts/Controller/Flash.js
@@ -1,11 +1,14 @@
 function FlashController(view){
-  this.id = view
-  this.view = $('#'+view)
-  this.dynamic_flash = $($.parseHTML("<div></div>"))
-  this.sticky_flash = $($.parseHTML("<div></div>"))
-  this.view.append(this.dynamic_flash)
-  this.view.append(this.sticky_flash)
-  this.count = 0
+  this.id = view;
+  this.view = $('#'+view);
+  this.dynamic_flash = $($.parseHTML("<div></div>"));
+  this.sticky_flash = $($.parseHTML("<div></div>"));
+  this.notifications = $($.parseHTML("<div class='flash_notifications'></div>"));
+  this.view.append(this.dynamic_flash);
+  this.view.append(this.sticky_flash);
+  this.view.parent().append(this.notifications);
+  this.notification_queue = [];
+  this.count = 0;
 }
 
 FlashController.prototype.get_alert_id = function(){
@@ -17,6 +20,7 @@ FlashController.prototype.add = function(text, type, timeout, where){
   var alert_id = this.get_alert_id()
   var html = "<div style='text-align:left' class='alert alert-"+type+"' data-flash-id="+alert_id+"><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><p>"+text+"</p></div>"
   var element = $($.parseHTML(html));
+  var notification = element.clone();
   element.hide()
   where.prepend(element)
   element.slideDown()
@@ -25,6 +29,31 @@ FlashController.prototype.add = function(text, type, timeout, where){
       //$('*[data-flash-id="'+alert_id+'"]').remove();
       element.slideUp()
     }, timeout*1000)
+  }
+  this.show_notification(notification);
+}
+
+FlashController.prototype.show_notification = function(notification){
+  this.notification_queue.push(notification);
+  // No notifications are being shown so we do it
+  // Detected by seeing we're alone in the queue
+  if(this.notification_queue.length == 1){
+    console.log("Starting queue processing");
+    this.show_next_notification();
+  }
+}
+
+FlashController.prototype.show_next_notification = function(){
+  var self = this;
+  var notification = this.notification_queue[0];
+  if(notification){
+    this.notifications.append(notification);
+    setTimeout(function(){
+      notification.fadeOut(function(){
+        self.notification_queue.shift();
+        self.show_next_notification();
+      });
+    },3000);
   }
 }
 
@@ -54,4 +83,21 @@ FlashController.prototype.sticky_warning = function(text){
 
 FlashController.prototype.empty = function(){
   this.dynamic_flash.html("")
+}
+
+FlashController.prototype.toggle_maximize = function(){
+  if(this.view.is(":visible")){
+    this.minimize();
+  }
+  else {
+    this.maximize();
+  }
+}
+
+FlashController.prototype.maximize = function(){
+  this.view.fadeIn();
+}
+
+FlashController.prototype.minimize = function(){
+  this.view.fadeOut();
 }
--- app/assets/javascripts/Controller/TopMenu.js
+++ app/assets/javascripts/Controller/TopMenu.js
@@ -10,13 +10,10 @@ TopMenuController.prototype.toggle_mobile_menu = function(){
     self.$.find('.extended_menu').slideUp(function(){
       // ensure editor is properly placed in case window was resized with menu opened
       self.editor.$editor.css('top', self.$.height() + "px");
-      self.flash.view.fadeIn();
     });
   }
   else {
-    self.flash.view.hide(function(){
-      self.$.find('.extended_menu').slideDown();
-    });
+    self.$.find('.extended_menu').slideDown();
   }
   self.full_menu_displayed = !self.full_menu_displayed;
 
