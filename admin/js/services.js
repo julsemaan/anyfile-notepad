b86d23acafeca6e229994b87c3e3244dc46f8fa4
Julien Semaan <julien@semaan.ca>
2016-07-20 18:08:11 -0400

    working generalized CRUDResource

--- admin/js/services.js
+++ admin/js/services.js
@@ -1,9 +1,71 @@
-angular.module('afnAdminApp.services', []).factory('MimeType', function($resource, $q) {
+angular.module('afnAdminApp.services', [])
+.factory('$CRUDResource', function($resource, $q){
+  
+  return function CRUDResourceFactory(res) {
+    res.formatObject = function(object) {
+      object["__display_attr__"] = object[object.__proto__.display_attr];
+      return object;
+    };
+
+    var super_query = res.query;
+    res.query = function() {
+      var defer = $q.defer();
+      var reply = []
+      super_query.call(this, arguments).$promise.then(function(objects){
+        for(var i in objects) {
+          objects[i] = res.formatObject(objects[i]);
+          reply.push(objects[i]);
+        }
+        defer.resolve(objects);
+      }, function() {defer.reject()});
+      reply.$promise = defer.promise;
+      return reply;
+    }
+
+    var super_get = res.get;
+    res.get = function(args) {
+      var defer = $q.defer();
+      var reply = new res();
+      super_get.call(this, args).$promise.then(function(object){
+        object = res.formatObject(object);
+        var promise = reply.$promise;
+        res.shallowClearAndCopy(object, reply);
+        reply.$promise = promise;
+        defer.resolve(object);
+      });
+      reply.$promise = defer.promise;
+      return reply;
+    }
+    
+    res.shallowClearAndCopy = function(src, dst) {
+      dst = dst || {};
+
+      angular.forEach(dst, function(value, key) {
+        delete dst[key];
+      });
+
+      for (var key in src) {
+        if (src.hasOwnProperty(key) && !(key.charAt(0) === '$' && key.charAt(1) === '$')) {
+          dst[key] = src[key];
+        }
+      }
+
+      return dst;
+    }
+    return res;
+  }
+
+
+  return res;
+})
+.factory('MimeType', function($resource, $CRUDResource) {
   var res = $resource('http://devbox.home.semaan.ca:8080/mime_types/:id', {id: '@id'}, {
     update: {
       method: 'PUT'
     }
   });
+  res = $CRUDResource(res);
+
   res.prototype.model_name = "MimeType";
   res.prototype.model_name_pl = "MimeTypes";
 
@@ -12,67 +74,16 @@ angular.module('afnAdminApp.services', []).factory('MimeType', function($resourc
 
   res.prototype.display_attr = "type_name";
 
-  res.formatObject = function(object) {
-    object["__display_attr__"] = object[object.__proto__.display_attr];
-    return object;
-  };
-
-  var super_query = res.query;
-  res.query = function() {
-    var defer = $q.defer();
-    var reply = []
-    super_query.call(this, arguments).$promise.then(function(objects){
-      for(var i in objects) {
-        objects[i] = res.formatObject(objects[i]);
-        reply.push(objects[i]);
-      }
-      defer.resolve(objects);
-    }, function() {defer.reject()});
-    reply.$promise = defer.promise;
-    return reply;
-  }
-
-  var super_get = res.get;
-  res.get = function(args) {
-    var defer = $q.defer();
-    var reply = new res();
-    super_get.call(this, args).$promise.then(function(object){
-      console.log(object)
-      object = res.formatObject(object);
-      var promise = reply.$promise;
-      res.shallowClearAndCopy(object, reply);
-      reply.$promise = promise;
-      defer.resolve(object);
-    });
-    reply.$promise = defer.promise;
-    console.log(super_get.call(this, args))
-    console.log(reply)
-    return reply;
-  }
-  
-  res.shallowClearAndCopy = function(src, dst) {
-    dst = dst || {};
-
-    angular.forEach(dst, function(value, key) {
-      delete dst[key];
-    });
-
-    for (var key in src) {
-      if (src.hasOwnProperty(key) && !(key.charAt(0) === '$' && key.charAt(1) === '$')) {
-        dst[key] = src[key];
-      }
-    }
-
-    return dst;
-  }
 
   return res;
-}).factory('Syntax', function($resource) {
+}).factory('Syntax', function($resource, $CRUDResource) {
   var res = $resource('http://devbox.home.semaan.ca:8080/syntaxes/:id', {id: '@id'}, {
     update: {
       method: 'PUT'
     }
   });
+  res = $CRUDResource(res);
+
   res.prototype.model_name = "Syntax";
   res.prototype.model_name_pl = "Syntaxes";
 
@@ -81,12 +92,14 @@ angular.module('afnAdminApp.services', []).factory('MimeType', function($resourc
 
   res.prototype.display_attr = "display_name";
   return res;
-}).factory('Extension', function($resource, Syntax, MimeType){
+}).factory('Extension', function($resource, $CRUDResource, Syntax, MimeType){
   var res = $resource('http://devbox.home.semaan.ca:8080/extensions/:id', {id: '@id'}, {
     update: {
       method: 'PUT'
     }
   });
+  res = $CRUDResource(res);
+
   res.prototype.model_name = "Extension";
   res.prototype.model_name_pl = "Extensions";
 
@@ -106,7 +119,6 @@ angular.module('afnAdminApp.services', []).factory('MimeType', function($resourc
         return $window.confirm(message);
     }
 }).service('$flash', function($timeout){
-  console.log(this)
   this.flash = [];
   
   this.add = function(type, msg, timeout) {
