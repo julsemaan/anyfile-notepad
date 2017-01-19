18a86f1575cb890a410bfae3dce7e849374d7216
Julien Semaan <jul.semaan@gmail.com>
2017-01-19 18:29:33 -0500

    started app main cleanup + locale clean

---
+++ client/assets/js/Controller/Locale.js
@@ -0,0 +1,26 @@
+Class("LocaleController", ["Model"]);
+
+LocaleController.prototype.init = function() {
+  var self = this;
+  self.nameMap = {};
+  self.translations = {};
+  self.findLocale();
+}
+
+LocaleController.prototype.addLocale = function(id, name, translations) {
+  var self = this;
+  self.nameMap[id] = name;
+  self.translations[id] = translations;
+  String.toLocaleString(self.translations);
+}
+
+LocaleController.prototype.findLocale = function() {
+  var desired_locale = getCookie("locale");
+  if(desired_locale && desired_locale != "auto"){
+    String.locale = desired_locale;
+  }
+}
+
+LocaleController.prototype.i18n = function(string) {
+  return string.toLocaleString();
+}
