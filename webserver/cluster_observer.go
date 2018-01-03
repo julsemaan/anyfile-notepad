7fb82dbe9e75b1162f9fb1e2aaf876f9745dd5ee
Julien Semaan <jul.semaan@gmail.com>
2018-01-02 19:51:51 -0500

    move golongpoll client code to its own lib

--- webserver/cluster_observer.go
+++ webserver/cluster_observer.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"net/url"
-	"time"
+
+	"github.com/julsemaan/golongpoll-client"
 )
-
-type PollResponse struct {
-	Events    []PollEvent `json:"events"`
-	Timestamp int64       `json:"timestamp"`
-}
-
-// Taken from https://github.com/jcuga/golongpoll/blob/master/events.go
-type PollEvent struct {
-	// Timestamp is milliseconds since epoch to match javascrits Date.getTime()
-	Timestamp int64  `json:"timestamp"`
-	Category  string `json:"category"`
-	// NOTE: Data can be anything that is able to passed to json.Marshal()
-	Data interface{} `json:"data"`
-}
 
 type ClusterObserver struct {
 	Hosts []string
@@ -32,49 +17,19 @@ func NewClusterObserver(hosts []string) *ClusterObserver {
 
 func (co *ClusterObserver) Start() {
 	for _, host := range co.Hosts {
-		fmt.Println("Now observing changes on", host)
 		go func(host string) {
-			since := time.Now().Unix() * 1000
+			fmt.Println("Now observing changes on", host)
+			u, err := url.Parse(host + "/events")
+			if err != nil {
+				fmt.Println("ERROR while parsing cluster URL", u, err)
+			}
+
+			client := glpclient.NewClient(u, "reload")
+			client.Start()
+
 			for {
-				pr := co.fetchEvents(host, since)
-				if len(pr.Events) > 0 {
-					fmt.Println("Got", len(pr.Events), "events")
-					for _, event := range pr.Events {
-						since = event.Timestamp
-						subscriptions.Reload()
-					}
-				} else {
-					// Only push timestamp forward if its greater than the last we checked
-					if pr.Timestamp > since {
-						since = pr.Timestamp
-					}
-				}
+				<-client.EventsChan
 			}
 		}(host)
 	}
-}
-
-func (co ClusterObserver) fetchEvents(host string, since int64) PollResponse {
-	fmt.Println("Checking for changes events since", since)
-	resp, err := http.Get(host + "/events?" + url.Values{
-		"category":   {"reload"},
-		"since_time": {fmt.Sprintf("%d", since)},
-		"timeout":    {"30"},
-	}.Encode())
-	if err != nil {
-		fmt.Println("Error while connecting to", host, "to observe changes", err)
-		// Wait 30 seconds before retrying
-		time.Sleep(30 * time.Second)
-	}
-
-	decoder := json.NewDecoder(resp.Body)
-	defer resp.Body.Close()
-
-	var pr PollResponse
-	decoder.Decode(&pr)
-	if err != nil {
-		fmt.Println("Error while decoding poll response", err)
-	}
-
-	return pr
 }
