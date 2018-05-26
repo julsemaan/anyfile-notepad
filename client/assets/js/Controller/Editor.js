a8c7d8d72e7304583eb7a48adcfa9d4c2bd0d299
Julien Semaan <jul.semaan@gmail.com>
2018-05-25 20:23:38 -0400

    add option to download a file

--- client/assets/js/Controller/Editor.js
+++ client/assets/js/Controller/Editor.js
@@ -257,6 +257,23 @@ EditorController.prototype.save = function(){
   return false;
 }
 
+EditorController.prototype.download = function(){
+  var self = this;
+	var filename = self.file.title;
+	var blob = new Blob([self.file.data], {type: self.file.mime_type});
+	if(window.navigator.msSaveOrOpenBlob) {
+			window.navigator.msSaveBlob(blob, filename);
+	}
+	else{
+			var elem = window.document.createElement('a');
+			elem.href = window.URL.createObjectURL(blob);
+			elem.download = filename;        
+			document.body.appendChild(elem);
+			elem.click();
+			document.body.removeChild(elem);
+	}
+}
+
 EditorController.prototype.activate_autosave = function(force){
   var self = this
   this.deactivate_autosave()
