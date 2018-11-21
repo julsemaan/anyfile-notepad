58e341ed228a82d3b9ec3998d6b9d5150a74b3cb
Julien Semaan <jul.semaan@gmail.com>
2018-11-20 20:31:14 -0500

    fix logic

--- webserver/http_handler.go
+++ webserver/http_handler.go
@@ -72,7 +72,7 @@ func (h Handler) ServeStaticApplication(w http.ResponseWriter, r *http.Request) 
 			}
 
 			if subscription := subscriptions.GetSubscription(userId); subscription != nil {
-				if subscriptions.CanHaveAccess(subscription) {
+				if !subscriptions.CanHaveAccess(subscription) {
 					fmt.Println(userId, "subscription isn't valid anymore")
 				} else {
 					fmt.Println(userId, "allowing access to ++ app")
