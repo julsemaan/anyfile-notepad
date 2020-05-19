4035c15f22c3aa3c2a56e79c64c26aa766a6f779
Julien Semaan <jul.semaan@gmail.com>
2020-05-19 19:54:43 -0400

    fix webdev server

--- afn-app.sh
+++ afn-app.sh
@@ -277,7 +277,9 @@ fi
 function start_server() {
   export AFN_PROD_APP_PATH="$COMPILED_APP"
   export AFN_DEV_APP_PATH="$COMPILED_APP"
-  find webserver/ -maxdepth 1 -name '*.go' ! -name '*_test.go' | xargs go run 2>&1 &
+  cd webserver/
+  find . -maxdepth 1 -name '*.go' ! -name '*_test.go' | xargs go run 2>&1 &
+  cd -
   WEB_PID=$!
   echo $WEB_PID > $WEB_PID_FILE
 }
