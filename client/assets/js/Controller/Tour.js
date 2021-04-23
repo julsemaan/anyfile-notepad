11e97d20dad3ab6033b4b52b6108ee38ea1d2e23
Julien Semaan <jul.semaan@gmail.com>
2021-04-22 20:28:49 -0400

    remove references to FB community

--- client/assets/js/Controller/Tour.js
+++ client/assets/js/Controller/Tour.js
@@ -78,7 +78,7 @@ TourController.prototype.menu_change_tour = function() {
   });
 
   tour.addStep('present-changes-completed', {
-    text: 'We hope you like those changes, and feel free to provide feedback on how we can make the app better via our Facebook Community.',
+    text: 'We hope you like those changes, and feel free to provide feedback via our contact option in the help menu.',
     attachTo: '#expanded_menu right',
     when: {
       hide: function() {
