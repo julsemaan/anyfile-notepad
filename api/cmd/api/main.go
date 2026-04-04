164766d91fe24e29363b672e561c8ecb9aebc6d3
Julien Semaan <jul.semaan@gmail.com>
2026-04-04 19:31:48 -0400

    feat(api): add main entry point and fix gitignore pattern

---
+++ api/cmd/api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+
+	"github.com/julsemaan/anyfile-notepad/api/internal/app"
+)
+
+func main() {
+	if err := app.Run(app.LoadConfigFromEnv()); err != nil {
+		log.Fatal(err)
+	}
+}
