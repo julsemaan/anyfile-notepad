5988fdb274fb8d24ef5ec57f81493728f3847150
Julien Semaan <jul.semaan@gmail.com>
2026-04-14 00:03:55 +0000

    fix(logging): standardize error output and fatal exit behavior

--- api/internal/logging/logging.go
+++ api/internal/logging/logging.go
@@ -1,16 +1,33 @@
 package logging
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const errorPrefix = "ERROR:"
+
+func output(message string) {
+	_ = log.Default().Output(3, message)
+}
 
 func Error(args ...interface{}) {
-	args = append([]interface{}{"ERROR:"}, args...)
-	log.Println(args...)
+	msg := strings.TrimSuffix(fmt.Sprintln(args...), "\n")
+	if msg == "" {
+		output(errorPrefix)
+		return
+	}
+
+	output(errorPrefix + " " + msg)
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Printf("ERROR: "+format, args...)
+	output(errorPrefix + " " + fmt.Sprintf(format, args...))
 }
 
 func Fatalf(format string, args ...interface{}) {
-	log.Fatalf("ERROR: "+format, args...)
+	output(errorPrefix + " " + fmt.Sprintf(format, args...))
+	os.Exit(1)
 }
