aa57f51bb51aa89684f26a6cdbefd49b612efa12
Julien Semaan <jul.semaan@gmail.com>
2026-06-08 01:07:05 +0000

    feat(stats): record and decode file-edit MIME types

--- client/assets/js/Model/Stat.js
+++ client/assets/js/Model/Stat.js
@@ -8,6 +8,19 @@ Stat.prototype.post_init = function(args){
 
 Class("StatIncrement", ["Stat"]);
 
+StatIncrement.hex_encode = function(str) {
+  var hex = '';
+  for (var i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    hex += code.toString(16);
+  }
+  return hex;
+}
+
+StatIncrement.record_file_edit_mime_type = function(mime_type) {
+  StatIncrement.record("file-edit.mime-type.hex." + StatIncrement.hex_encode(mime_type));
+}
+
 StatIncrement.record = function(key) {
   var self = new StatIncrement({key:key});
   self.type = "increment";
---
+++ script/decode_hex_mime_stats.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# decode_hex_mime_stats.pl - Extract hex-encoded MIME types from app stats logs
+#
+# Usage:
+#   docker compose logs api | perl script/decode_hex_mime_stats.pl
+#   perl script/decode_hex_mime_stats.pl app.log
+#   echo '<log line>' | perl script/decode_hex_mime_stats.pl
+#
+# Scans each input line for the stat key prefix:
+#   afn.app.file-edit.mime-type.hex.<hex-encoded-mime>
+# and prints the decoded MIME type on stdout.
+#
+# Non-matching lines are silently ignored.
+# Malformed hex values produce a warning on stderr and continue.
+
+use strict;
+use warnings;
+
+my $matched = 0;
+
+while (my $line = <>) {
+    while ($line =~ /afn\.app\.file-edit\.mime-type\.hex\.([0-9A-Fa-f]+)/g) {
+        my $hex = $1;
+        if ($hex !~ /^[0-9A-Fa-f]+$/) {
+            warn "decode_hex_mime_stats.pl: warning: malformed hex '$hex' at line $.\n";
+            next;
+        }
+        my $decoded = pack("H*", $hex);
+        print "$decoded\n";
+        $matched = 1;
+    }
+}
+
+exit 0;
